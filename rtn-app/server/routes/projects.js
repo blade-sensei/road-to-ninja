f186c0dfa5c48570c8f576677f4983b0f053850a
coyla <jcoyla123@gmail.com>
2018-01-29 22:23:39 +0100

    Add database connexion to mongodb

--- rtn-app/server/routes/projects.js
+++ rtn-app/server/routes/projects.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+//database server
+
+mongoose.connect('mongodb://localhost/test');
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('database connexion success!');
+});
 
 router.get('/', (req,res) => {
-  res.send('route projects');
+
+
+  let schema = new mongoose.Schema({ name: String});
+  let User = mongoose.model("homes", schema);
+  User.find((err, docs) => {
+    res.send(docs);
+  });
 });
 
 module.exports = router;
--- rtn-app/server/server.js
+++ rtn-app/server/server.js
@@ -4,13 +4,12 @@ const projects = require('./routes/projects');
 const index = require('./routes/index');
 
 
+//web server
 app.use('/', index);
 app.use('/projects',projects);
 app.use(function(req, res){
   res.send(404);
 });
-
-
 const port = process.env.port || '3000';
 app.listen(port, () => {
   console.log(`server running and listening on : ${port} ...`);
