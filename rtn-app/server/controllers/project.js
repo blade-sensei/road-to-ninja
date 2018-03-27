a23812210c955c0c6770a6c692c49b6ec3ad0a64
coyla <jcoyla123@gmail.com>
2018-03-27 23:24:32 +0200

    Create controller to split user and project routes

--- rtn-app/server/controllers/project.js
+++ rtn-app/server/controllers/project.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user');
 const logger = require('../helpers/logger');
 const token = require('../middlewares/token');
 const jwt = require('jsonwebtoken');
-
-//models
 const userModel = require('../models/user.model');
 const projectModel = require('../models/project.model');
 
-router.use('/user', userController);
-module.exports = router;
+
+router.get('/projects', token.verifyToken, token.isAuthorized, (req,res) => {
+  projectModel.find((err, docs) => {
+    res.send(docs);
+  });
+});
