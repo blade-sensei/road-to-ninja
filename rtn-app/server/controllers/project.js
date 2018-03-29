7c048e88109c3a092d8d49980f871a5119978e8d
coyla <jcoyla123@gmail.com>
2018-03-29 22:14:46 +0200

    Open routes than dont need token and user authentication

--- rtn-app/server/controllers/project.js
+++ rtn-app/server/controllers/project.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const token = require('../middlewares/token');
 const projectModel = require('../models/project.model');
 
 
-router.get('', token.verifyToken, token.isAuthorized, (req,res) => {
+router.get('', (req,res) => {
   projectModel.find((err, docs) => {
     res.send(docs);
   });
