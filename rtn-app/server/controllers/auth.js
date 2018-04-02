5bd13154c6eb99149d5fc1b652ac3b6e9407ba50
coyla <jcoyla123@gmail.com>
2018-04-03 00:43:27 +0200

    Fix all lint errors

--- rtn-app/server/controllers/auth.js
+++ rtn-app/server/controllers/auth.js
@@ -1,24 +1,33 @@
 const express = require('express');
-const router = express.Router();
 const jwt = require('jsonwebtoken');
 const userModel = require('../models/user.model');
 const verifyReq = require('../helpers/request');
 
+const router = express.Router();
 router.post('/login', (req, res, next) => {
   const requiredParameters = ['username', 'password'];
-  verifyReq.hasRequiredParameters(requiredParameters, req.body) ? next() :
+  if (verifyReq.hasRequiredParameters(requiredParameters, req.body)) {
+    next();
+  } else {
     res.status(404).send('Some of this required parameters are missing: '
-      .concat(requiredParameters.join(', ')))
+      .concat(requiredParameters.join(', ')));
+  }
 }, (req, res) => {
-  let query = userModel.where({username : req.body.username, password: req.body.password});
-  query.findOne((err, user) =>{
+  const query = userModel.where({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  query.findOne((err, user) => {
     const tokenExpirationTime = 60 * 60 * 24 * 1000;
-    if(!user){
+    if (!user) {
       res.status(401).send('Login fail');
     } else {
-      let token = jwt.sign({iss : 'rtn-token', id: user.uid, admin: user.admin},
-        req.app.get('secret_key'),{expiresIn : tokenExpirationTime});
-      res.json({username : user.username, logged : true, token : token});
+      const token = jwt.sign(
+        { iss: 'rtn-token', id: user.uid, admin: user.admin },
+        req.app.get('secret_key'),
+        { expiresIn: tokenExpirationTime },
+      );
+      res.json({ username: user.username, logged: true, token });
     }
   });
 });
