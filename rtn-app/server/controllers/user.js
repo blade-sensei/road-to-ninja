c8c3884d260d58113320d9f1ee897a28b542d1de
coyla <jcoyla123@gmail.com>
2018-04-03 10:48:01 +0200

    fix lint errors for user.js file

--- rtn-app/server/controllers/user.js
+++ rtn-app/server/controllers/user.js
@@ -1,71 +1,91 @@
 const express = require('express');
-const router = express.Router();
-const logger = require('../helpers/logger');
 const token = require('../middlewares/token');
 const userModel = require('../models/user.model');
-const projectModel = require('../models/project.model');
+const ProjectModel = require('../models/project.model');
 const verifyReq = require('../helpers/request');
 
+const router = express.Router();
 
-router.get('', (req,res) => {
-  userModel.find((err, docs) => {
-    logger.debug(docs);
-    res.send(docs);
-  });
+router.get('', (req, res) => {
+  userModel.find((err, docs) => res.send(docs));
 });
 
-//user projects
+// user projects
 router.get('/:uid/projects', (req, res) => {
-  if (req.query.title){
-    projectModel.find({uid: req.params.uid, title:req.query.title}, (error, docs) => {
-      if (error){
-        res.send("Bad request or database problem.");
-      } else {
-        res.status(200);
-        res.send(docs);
-      }
-    })
+  if (req.query.title) {
+    ProjectModel.find(
+      { uid: req.params.uid, title: req.query.title },
+      (error, docs) => {
+        if (error) {
+          res.send('Bad request or database problem.');
+        } else {
+          res.status(200);
+          res.send(docs);
+        }
+      },
+    );
   } else {
-    projectModel.find({uid : req.params.uid}, (error, docs) => {
-      if(error){
-        res.send("Bad request or database problem.");
+    ProjectModel.find({ uid: req.params.uid }, (error, docs) => {
+      if (error) {
+        res.send('Bad request or database problem.');
       } else {
         res.status(200);
         res.send(docs);
       }
-    })
+    });
   }
 });
 
-router.post('/:uid/projects', token.verifyToken, token.isAuthorized, (req, res, next) => {
-  let requiredParameters = ['title'];
-  verifyReq.hasRequiredParameters(requiredParameters, req.body) ? next() :
-    res.status(404).send('Some of these required parameters are missing : '
-      .concat(requiredParameters.join(', ')));
-}, (req, res) => {
-  new projectModel(req.body).save().then( project => res.send(project))
-    .catch( err => { res.status(500).send(`database error ${err}`);
-    });
-});
+router.post(
+  '/:uid/projects',
+  token.verifyToken,
+  token.isAuthorized,
+  (req, res, next) => {
+    const requiredParameters = ['title'];
+    if (verifyReq.hasRequiredParameters(requiredParameters, req.body)) {
+      next();
+    } else {
+      res.status(404).send('Some of these required parameters are missing : '
+        .concat(requiredParameters.join(', ')));
+    }
+  },
+  (req, res) => {
+    new ProjectModel(req.body).save()
+      .then(project => res.send(project))
+      .catch(err => res.status(500).send(`database error ${err}`));
+  },
+);
 
-router.patch('/:uid/projects/:id', token.verifyToken, token.isAuthorized, (req, res, next) => {
-  let requiredParameters = ['title'];
-  verifyReq.hasRequiredParameters(requiredParameters, req.body) ? next() :
-    res.status(404).send('Some of these required parameters are missing : '
-      .concat(requiredParameters.join(', ')));
-}, (req, res) => {
-  let project = Object.assign({}, req.body);
-  projectModel.findOneAndUpdate({'uid':req.params.uid, '_id':req.params.id},
-    project, {new: true}, (err, project) => {
-      if(project){
-        res.status(200);
-        res.send(project);
-      }
-      else{
-        res.status(500);
-        res.send('Project not found or couldn\'t save.');
-      }
-    });
-});
+router.patch(
+  '/:uid/projects/:id',
+  token.verifyToken,
+  token.isAuthorized,
+  (req, res, next) => {
+    const requiredParameters = ['title'];
+    if (verifyReq.hasRequiredParameters(requiredParameters, req.body)) {
+      next();
+    } else {
+      res.status(404).send('Some of these required parameters are missing : '
+        .concat(requiredParameters.join(', ')));
+    }
+  },
+  (req, res) => {
+    const project = Object.assign({}, req.body);
+    ProjectModel.findOneAndUpdate(
+      { uid: req.params.uid, _id: req.params.id },
+      project,
+      { new: true },
+      (updatedProject) => {
+        if (project) {
+          res.status(200);
+          res.send(updatedProject);
+        } else {
+          res.status(500);
+          res.send('Project not found or couldn\'t save.');
+        }
+      },
+    );
+  },
+);
 
 module.exports = router;
