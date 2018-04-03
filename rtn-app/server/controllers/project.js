7c9586b6fc8bf08ffc1d89b0bdc0e64c940b715e
coyla <jcoyla123@gmail.com>
2018-04-03 09:01:26 +0200

    Fix lint errors for project.js file

--- rtn-app/server/controllers/project.js
+++ rtn-app/server/controllers/project.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const router = express.Router();
 const projectModel = require('../models/project.model');
 
+const router = express.Router();
 
-router.get('', (req,res) => {
-  projectModel.find((err, docs) => {
-    res.send(docs);
-  });
+router.get('', (req, res) => {
+  projectModel.find((err, docs) => res.send(docs));
 });
 
 module.exports = router;
