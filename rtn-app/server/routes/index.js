28384ae869e37450279e3345a4970aae2bd8dbc9
coyla <jcoyla123@gmail.com>
2018-04-03 21:18:40 +0200

    Fix lint errors of index route file

--- rtn-app/server/routes/index.js
+++ rtn-app/server/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const router = express.Router();
 const path = require('path');
 
-router.get('/', (req,res,next) => {
-  console.log(path.join(__dirname,'../'));
-  res.sendFile('index.html', {root: path.join(__dirname,'../')});
-});
+const router = express.Router();
+
+router.get('/', (req, res) => res.sendFile(
+  'index.html',
+  { root: path.join(__dirname, '../') },
+));
 
 module.exports = router;
