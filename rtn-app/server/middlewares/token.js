6bef8f8ae67ad92fe78e993df8d1876d7f9a19d1
coyla <jcoyla123@gmail.com>
2018-04-05 23:49:35 +0200

    Fix lint errors

--- rtn-app/server/middlewares/token.js
+++ rtn-app/server/middlewares/token.js
@@ -1,37 +1,33 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next)  => {
-  let token = req.body.token || req.query.token || req.headers['x-access-token']
+const verifyToken = (req, res, next) => {
+  const token = req.body.token || req.query.token || req.headers['x-access-token']
     || req.cookies.token;
   if (token) {
-    jwt.verify(token, req.app.get('secret_key'), (err, decoded) => {
+    return jwt.verify(token, req.app.get('secret_key'), (err, decoded) => {
       if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });
-      } else {
-        req.auth = decoded;
-        next();
+        res.json({ success: false, message: 'Failed to authenticate token.' });
       }
-    });
-  } else {
-    return res.status(403).send({
-      success: false,
-      message: 'No token provided.'
+      req.auth = decoded;
+      return next();
     });
   }
+  return res.status(403).send({
+    success: false,
+    message: 'No token provided.',
+  });
 };
 
 const isAuthorized = (req, res, next) => {
-    if (req.auth.admin === true){
-      next();
-    } else if (req.auth.id === req.params.uid){
-      next();
-    } else {
-      res.status(403);
-      res.send('You are not allowed to view this ressource.');
-    }
+  if (req.auth.admin === true) {
+    return next();
+  } else if (req.auth.id === req.params.uid) {
+    return next();
+  }
+  return res.sendStatus(403);
 };
 
 module.exports = {
-  'isAuthorized' : isAuthorized,
-  'verifyToken' : verifyToken
+  isAuthorized,
+  verifyToken,
 };
