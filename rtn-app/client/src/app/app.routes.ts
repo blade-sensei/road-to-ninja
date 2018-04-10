af7511556efe9f5eb6b372601727f19ee2a856aa
coyla <jcoyla123@gmail.com>
2018-04-10 23:09:23 +0200

    Add page for login form

--- rtn-app/client/src/app/app.routes.ts
+++ rtn-app/client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from "./components/user/user.component";
+import {AuthFormComponent} from "./components/auth/auth-form/auth-form.component";
 
 const routes : Routes = [
-  { path: 'users', component: UserComponent }
+  { path: 'users', component: UserComponent },
+  { path: 'login', component: AuthFormComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
