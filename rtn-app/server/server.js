6447874b7306066cbf3db6ca5e1b25aca9349090
Coyla <jcoyla123@gmail.com>
2018-04-17 09:50:45 +0200

    Add enable cors request to allow angular to request api

--- rtn-app/server/server.js
+++ rtn-app/server/server.js
@@ -1,47 +1,48 @@
 const express = require('express');
-const api = require('./routes/api');
-const index = require('./routes/index');
-const seed = require('./routes/seed');
-const logger = require('./helpers/logger');
 const mongoose = require('mongoose');
-const config = require('./config');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const api = require('./routes/api');
+const index = require('./routes/index');
+const seed = require('./routes/seed');
+const config = require('./config');
+const cors = require('./middlewares/cors');
+const logger = require('./helpers/logger');
 
 const app = express();
 
-// enable cross origin
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, ' +
-             'Content-Type, Accept, Authorization');
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+// enable cross origin
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+app.use(cors.enablePreFlightRequest);
+
 app.set('secret_key', config.auth.key);
 // adding routes modules
 app.use('/', index);
 app.use('/explorer', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', api);
 app.use('/seed', seed);
+app.use((req, res) => { res.send(404); });
+
 // config server
-app.use((req, res) => {
-  res.send(404);
-});
 const port = process.env.port || config.server.port;
 app.listen(port, () => {
-  logger.info(`server api is running on :${port}port ...`);
+  logger.info(`server api is running on : ${port} port ...`);
 });
 
 mongoose.connect(`mongodb://${config.db.host}/${config.db.name}`).then(() => {
   mongoose.set('debug', true);
-  logger.info(`database connection on ${mongoose.connection.port} ' + 
-              'port with success!`);
+  logger.info(`database connection on ${mongoose.connection.port} port with success!`);
 });
 
 module.exports = app;
