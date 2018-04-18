518b2abee0a21e57a9701022defd854362411a92
coyla <jcoyla123@gmail.com>
2018-04-18 10:46:24 +0200

    add error failed login handling in form

--- rtn-app/client/src/app/components/auth/auth-form/auth-form.component.ts
+++ rtn-app/client/src/app/components/auth/auth-form/auth-form.component.ts
@@ -11,6 +11,7 @@ export class AuthFormComponent implements OnInit {
 
   public username: string;
   public password: string;
+  public errors: string[] = [];
   constructor(
     private authenticationService: AuthenticationService,
     private router: Router,
@@ -29,6 +30,7 @@ export class AuthFormComponent implements OnInit {
         this.router.navigate(['/']);
       },
       error => {
+        this.errors.push(error.error);
         this.router.navigate(['/login']);
         //redirect login page with errors
       },
