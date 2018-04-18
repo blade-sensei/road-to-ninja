8d8a85a8ac90c553da25eddb6e5301f2b691af30
Pampacore <quentin.poure.pro@gmail.com>
2018-04-18 02:39:21 +0200

    Ajout fichier server.js modifié

--- rtn-app/server/server.js
+++ rtn-app/server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-app = express();
+const app = express();
 const api = require('./routes/api');
 const index = require('./routes/index');
 const seed = require('./routes/seed');
@@ -10,39 +10,36 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-
-
-
-
-//enable cross origin
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+// enable cross origin
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, ' +
+             'Content-Type, Accept, Authorization');
   next();
 });
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.set('secret_key', config.auth.key);
-//adding routes modules
+// adding routes modules
 app.use('/', index);
 app.use('/explorer', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/api',api);
-app.use('/seed',seed);
-//config server
-app.use(function(req, res){
+app.use('/api', api);
+app.use('/seed', seed);
+// config server
+app.use((req, res) => {
   res.send(404);
 });
 const port = process.env.port || config.server.port;
 app.listen(port, () => {
-  logger.info(`server api is running on : ${port} port ...` );
+  logger.info(`server api is running on :${port}port ...`);
 });
 
 mongoose.connect(`mongodb://${config.db.host}/${config.db.name}`).then(() => {
-    mongoose.set('debug', true);
-  logger.info(`database connection on ${mongoose.connection.port} port with success!`);
-  }
-);
+  mongoose.set('debug', true);
+  logger.info(`database connection on ${mongoose.connection.port} ' + 
+              'port with success!`);
+});
 
 module.exports = app;
