b15d09211347c434b728f5d3ce63a60dae1f554e
Pampacore <quentin.poure.pro@gmail.com>
2018-04-18 15:10:16 +0200

    Fix project.model.js lint

--- rtn-app/server/models/project.model.js
+++ rtn-app/server/models/project.model.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 
-let schema = new mongoose.Schema({
-  title : String,
-  description: {type : String, default : ''},
-  status : {type : String, default : 'not started' },
-  data : {type : String, default : 'http://'},
-  uid : String,
-  requires : Array
+const schema = new mongoose.Schema({
+  title: String,
+  description: { type: String, default: '' },
+  status: { type: String, default: 'not started' },
+  data: { type: String, default: 'http://' },
+  uid: String,
+  requires: Array,
 });
 
-let Project = mongoose.model("projects", schema);
+const Project = mongoose.model('projects', schema);
 
 module.exports = Project;
