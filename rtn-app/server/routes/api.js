a3de54220f9d8a54f138f5ba97a3d755f403761a
Coyla <jcoyla123@gmail.com>
2018-05-21 01:33:45 +0200

    refactor api.js newline after imports

--- rtn-app/server/routes/api.js
+++ rtn-app/server/routes/api.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const router = express.Router();
 const userController = require('../controllers/user');
 const authController = require('../controllers/auth');
 const projectController = require('../controllers/project');
 
-//models
+const router = express.Router();
+
 router.use('/users', userController);
 router.use('/auth', authController);
 router.use('/projects', projectController);
