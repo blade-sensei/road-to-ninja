8f71e5eaef9a079fb32b6ab5f0f82d1fe13c2f1f
Coyla <jcoyla123@gmail.com>
2018-05-22 00:03:52 +0200

    refactor auth-interceptor service

--- rtn-app/client/src/app/services/authentication/auth-interceptor.service.ts
+++ rtn-app/client/src/app/services/authentication/auth-interceptor.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs/Observable";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
 
   constructor() { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    if(currentUser && currentUser.token) {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser && currentUser.token) {
       req = req.clone({
         headers: req.headers.set('x-access-token', currentUser.token)
       });
