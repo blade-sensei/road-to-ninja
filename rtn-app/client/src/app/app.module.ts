71f9f2adf1d6aba6c6b82e7cfc8a3fab9782fc5b
Coyla <jcoyla123@gmail.com>
2018-05-21 23:58:47 +0200

    refactor app.routes. file nospaces

--- rtn-app/client/src/app/app.module.ts
+++ rtn-app/client/src/app/app.module.ts
@@ -1,21 +1,21 @@
-///<reference path="app.routes.ts"/>
+///<reference path='app.routes.ts'/>
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './components/app/app.component';
-import {UserService} from "./components/user/user.service";
-import { AppRoutingModule, routingComponents } from "./app.routes";
+import {UserService} from './components/user/user.service';
+import { AppRoutingModule, routingComponents } from './app.routes';
 import { UserProjectsComponent } from './components/user-projects/user-projects.component';
 import { ProjectInfoComponent } from './components/project-info/project-info/project-info.component';
-import {UserProjectsService} from "./components/user-projects/user-projects.service";
+import {UserProjectsService} from './components/user-projects/user-projects.service';
 import { MenuComponent } from './components/menu/menu.component';
-import {ModalModule} from "ngx-bootstrap";
+import {ModalModule} from 'ngx-bootstrap';
 import { ProjectAddComponent } from './components/project-add/project-add.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 import { AuthFormComponent } from './components/auth/auth-form/auth-form.component';
-import {AuthenticationService} from "./components/auth/authentication.service";
-import {AuthInterceptorService} from "./services/authentication/auth-interceptor.service";
-import {ProfileService} from "./services/profile.service";
+import {AuthenticationService} from './components/auth/authentication.service';
+import {AuthInterceptorService} from './services/authentication/auth-interceptor.service';
+import {ProfileService} from './services/profile.service';
 
 @NgModule({
   declarations: [
