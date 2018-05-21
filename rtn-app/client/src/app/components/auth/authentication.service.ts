bc2af1bd1dd232d3a749509892f3f34751148b0a
Coyla <jcoyla123@gmail.com>
2018-05-21 23:59:57 +0200

    refactor menu component

--- rtn-app/client/src/app/components/auth/authentication.service.ts
+++ rtn-app/client/src/app/components/auth/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs/Observable";
-import { HttpClient } from "@angular/common/http";
-import { Token } from "../../models/token";
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import { Token } from '../../models/token';
 
 @Injectable()
 export class AuthenticationService {
@@ -9,12 +9,12 @@ export class AuthenticationService {
   constructor(private httpClient: HttpClient) { }
 
   login(credentials): Observable<any> {
-    const endpoint = "http://localhost:3000/api/auth/login";
+    const endpoint = 'http://localhost:3000/api/auth/login';
     return this.httpClient.post(endpoint, credentials).map((token: Token) => {
-      if(token && token.token) {
+      if (token && token.token) {
         localStorage.setItem('currentUser', JSON.stringify(token));
       }
       return token;
-    })
+    });
   }
 }
