7588c2bb751e3e345bf47f96b7a4e5acb58f852c
Coyla <jcoyla123@gmail.com>
2018-05-22 00:03:11 +0200

    refactor user-projects service

--- rtn-app/client/src/app/components/user-projects/user-projects.service.ts
+++ rtn-app/client/src/app/components/user-projects/user-projects.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs/Observable";
-import {Project} from "../../models/project";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {Project} from '../../models/project';
 
 @Injectable()
 export class UserProjectsService {
 
-  constructor(private http : HttpClient) {}
-  getAllProjects(uid) : Observable<Project[]>{
+  constructor(private http: HttpClient) {}
+  getAllProjects(uid): Observable<Project[]> {
     return this.http.get<Project[]>(`http://localhost:3000/api/users/${uid}/projects`);
   }
 }
