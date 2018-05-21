f068679aad818d42a3ac69f5edc28b0ac4c68e16
Coyla <jcoyla123@gmail.com>
2018-05-22 00:03:28 +0200

    refactor user service ts

--- rtn-app/client/src/app/components/user/user.service.ts
+++ rtn-app/client/src/app/components/user/user.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class UserService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getUserByName(name) {
-    return this.http.get(`http://localhost:3000/api/users/${name}`)
+    return this.http.get(`http://localhost:3000/api/users/${name}`);
   }
 }
