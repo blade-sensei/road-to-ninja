2ddf16f811d74a2f43cfcac6c2e9faf52213e522
Coyla <jcoyla123@gmail.com>
2018-05-24 23:58:44 +0200

    add edit button when admin is logged in

--- rtn-app/client/src/app/components/user-projects/user-projects.component.ts
+++ rtn-app/client/src/app/components/user-projects/user-projects.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { UserProjectsService} from './user-projects.service';
+import {ProfileService} from "../../services/profile.service";
 
 @Component({
   selector: 'app-user-projects',
@@ -8,12 +9,21 @@ import { UserProjectsService} from './user-projects.service';
 })
 export class UserProjectsComponent implements OnInit {
   projects: any = [];
+  isUserLogged: boolean;
   @Input() user: any;
+
   constructor(private userProjectService: UserProjectsService) { }
   ngOnInit() {
     this.userProjectService.getAllProjects(this.user.uid).subscribe(projects => {
       this.projects = projects;
     });
+    this.isUserLogged = this.isUserLoggedIn();
+  }
+
+  isUserLoggedIn(): boolean {
+    console.log(ProfileService.getCurrentUser());
+    console.log(this.user);
+    return (ProfileService.getCurrentUser().username === this.user.username);
   }
 
 }
