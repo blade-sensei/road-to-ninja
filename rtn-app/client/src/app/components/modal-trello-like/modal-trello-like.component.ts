71e50e462a634dcd2cbe51a982177b715eb619bb
Coyla <jcoyla123@gmail.com>
2018-07-23 23:14:28 +0200

    [feature] update project:  change trelloLikeModal to version 2
    add service provider to trelloLike to communicate with external components

--- rtn-app/client/src/app/components/modal-trello-like/modal-trello-like.component.ts
+++ rtn-app/client/src/app/components/modal-trello-like/modal-trello-like.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap';
-import { ProjectEditComponent } from '../project-edit/project-edit.component';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ModalTrelloLikeService } from '../../services/modal-trello-like.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Project } from '../../models/project';
 
 @Component({
   selector: 'app-modal-trello-like',
   templateUrl: './modal-trello-like.component.html',
   styleUrls: ['./modal-trello-like.component.css']
 })
-export class ModalTrelloLikeComponent implements OnInit {
-  modalRef: BsModalRef;
-  constructor(private modalService: BsModalService) { }
+export class ModalTrelloLikeComponent implements OnInit, OnDestroy {
+  isModalOpen = false;
+  projectEdition: Project;
+
+  isOpenSubscription: Subscription;
+  projectSubscription: Subscription;
+  constructor(
+    private modalTrelloService: ModalTrelloLikeService,
+  ) {
+      this.isOpenSubscription = this.modalTrelloService.getOpenModalSource()
+        .subscribe(openModal => this.isModalOpen = openModal);
+      this.projectSubscription =  this.modalTrelloService.getProjectEdition()
+      .subscribe(project => this.projectEdition = project);
+    }
 
   ngOnInit() {
   }
 
-  showModal(initialState: object, component: any) {
-    this.modalRef = this.modalService.show(component, { initialState });
-    return this.modalRef;
+  ngOnDestroy() {
+    this.isOpenSubscription.unsubscribe();
+    this.projectSubscription.unsubscribe();
   }
+
+  getDisplayStyle() {
+    return this.isModalOpen ? 'block' : 'none';
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+
 
 }
--- rtn-app/client/src/app/services/modal-trello-like.service.ts
+++ rtn-app/client/src/app/services/modal-trello-like.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Subject } from 'rxjs/Subject';
+import { Project } from '../models/project';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ModalTrelloLikeService {
 
-  modalRef: BsModalRef;
-  constructor(private modalService: BsModalService) { }
+  private projectEditionSource = new Subject<Project>();
+  private openModalSource = new Subject<boolean>();
 
-  showModal(component: any, initialState: object) {
-    this.modalRef = this.modalService.show(component, { initialState });
-    return this.modalRef;
+  constructor() { }
+
+  setProjectEdition(project: Project) {
+    this.projectEditionSource.next(project);
   }
+
+  getProjectEdition(): Observable<Project> {
+    return this.projectEditionSource.asObservable();
+  }
+
+  setOpenModalSource(isOpen: boolean) {
+    this.openModalSource.next(isOpen);
+  }
+
+  getOpenModalSource(): Observable<boolean> {
+    return this.openModalSource.asObservable();
+  }
+
 }
