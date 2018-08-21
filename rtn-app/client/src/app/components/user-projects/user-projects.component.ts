7ee7efc291f67af47cac7b106a0569252b1d363c
Coyla <jcoyla123@gmail.com>
2018-08-21 23:34:35 +0200

    fix(front): unsyncro user projects when project is added (#47)
    when we edit there is reference from user project to info project to edit component so no service provider needed. But when we add there is no reference.
    
    close #47

--- rtn-app/client/src/app/components/user-projects/user-projects.component.ts
+++ rtn-app/client/src/app/components/user-projects/user-projects.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UserProjectsService } from './user-projects.service';
 import { ProfileService } from '../../services/profile/profile.service';
+import { Subscription } from 'rxjs/Subscription';
+import { ModalTrelloLikeService } from '../../services/modal-trello-like/modal-trello-like.service';
+import { Project } from '../../models/project';
 
 @Component({
   selector: 'app-user-projects',
@@ -11,10 +14,15 @@ export class UserProjectsComponent implements OnInit {
   projects: any = [];
   isUserLogged: boolean;
   @Input() user: any;
+  projectToAddSavedSubscription: Subscription;
 
-  constructor(private userProjectService: UserProjectsService) { }
+  constructor(
+    private userProjectService: UserProjectsService,
+    private modalTrelloLikeService: ModalTrelloLikeService
+  ) { }
 
   ngOnInit() {
+    this.subscribeToProjectAddSaved();
     this.userProjectService.getUserProjects(this.user.uid)
       .subscribe(projects => this.projects = projects);
     this.isUserLogged = this.isUserLoggedIn();
@@ -26,6 +34,13 @@ export class UserProjectsComponent implements OnInit {
       return ProfileService.getCurrentUserToken().username === this.user.username;
     }
     return false;
+  }
+
+  subscribeToProjectAddSaved() {
+    this.projectToAddSavedSubscription = this.modalTrelloLikeService
+      .getProjectToAddSaved().subscribe(project => {
+        this.projects.push(project);
+      });
   }
 
 }
