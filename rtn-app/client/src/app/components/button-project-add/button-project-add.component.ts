aafdb283ad0f8775b667060202c3cbd1561fc529
Coyla <jcoyla123@gmail.com>
2018-08-25 22:00:04 +0200

    fix(front): creation must be set before event project edit
    its important cause setporject edit activate dynamic component creation, which pass creationmode, so if creationmode is not set before this is always false (update mode)

--- rtn-app/client/src/app/components/button-project-add/button-project-add.component.ts
+++ rtn-app/client/src/app/components/button-project-add/button-project-add.component.ts
@@ -18,8 +18,8 @@ export class ButtonProjectAddComponent implements OnInit {
 
   showAddProjectModal() {
     const project = new Project();
+    this.modalTrelloLikeService.setIsCreationMode(true);
     this.modalTrelloLikeService.setProjectToEdit(project);
     this.modalTrelloLikeService.setIsOpenModal(true);
-    this.modalTrelloLikeService.setIsCreationMode(true);
   }
 }
