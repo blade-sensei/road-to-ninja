223dbe082a9fa1e947ae332306854c26c813aa5d
Coyla <jcoyla123@gmail.com>
2018-09-05 17:08:44 +0200

    chore(services): add filter service to enable input, status filter

---
+++ rtn-app/client/src/app/services/filter/filter-projects.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { FilterProjectsService } from './filter-projects.service';
+
+describe('FilterProjectsService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [FilterProjectsService]
+    });
+  });
+
+  it('should be created', inject([FilterProjectsService], (service: FilterProjectsService) => {
+    expect(service).toBeTruthy();
+  }));
+});
