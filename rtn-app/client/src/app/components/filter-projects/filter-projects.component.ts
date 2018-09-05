68bfb3a28def4da73c7574c714d41296ea98f89d
Coyla <jcoyla123@gmail.com>
2018-09-05 17:11:17 +0200

    feature(front): add filter and search system for user projects (#51)
    close #51

--- rtn-app/client/src/app/components/filter-projects/filter-projects.component.ts
+++ rtn-app/client/src/app/components/filter-projects/filter-projects.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserProjectsService } from '../user-projects/user-projects.service';
+import { UserService } from '../user/user.service';
+import { User } from '../../models/user';
+import { FilterProjectsService } from '../../services/filter/filter-projects.service';
 
 @Component({
   selector: 'app-filter-projects',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-projects.component.css']
 })
 export class FilterProjectsComponent implements OnInit {
-
-  constructor() { }
+  searchUserInput = '';
+  statusFilter = '';
+  constructor(
+    private userProjectsService: UserProjectsService,
+    private userService: UserService,
+    private filterService: FilterProjectsService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  onFilterProjects() {
+    const options = {};
+    if (!(this.statusFilter === '')) {
+      const filter = { status: this.statusFilter };
+      Reflect.set(options, 'filter', filter);
+    }
+
+    if (this.searchUserInput) {
+      Reflect.set(options, 'search', this.searchUserInput);
+    }
+
+    this.userService.getCurrentUser().subscribe((user: User) => {
+      this.userProjectsService
+        .getUserProjects(user.uid, options)
+        .subscribe(projects => {
+          this.filterService.setFilteredProjects(projects);
+        });
+    });
   }
 
 }
