935c68c879a95cff46949c86fd13ae8ab3696d71
Coyla <jcoyla123@gmail.com>
2018-09-18 23:27:46 +0200

    chore(projects): change requirement type, to array of project ids
    used to better management of required proejcts changes

--- rtn-app/server/helpers/seeds/project.feed.js
+++ rtn-app/server/helpers/seeds/project.feed.js
@@ -1,5 +1,10 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Types;
+
 const data = [
   {
+    _id: ObjectId('5b9e1794a626cddcb03a6df5'),
     title: 'project 1',
     description: 'This is the description for project 1',
     status: 'finished',
@@ -8,6 +13,7 @@ const data = [
     requires: [],
   },
   {
+    _id: ObjectId('5ba0f22cb0ad9d0908ece391'),
     title: 'project 1b',
     description: 'This is the description for project 1b',
     status: 'finished',
@@ -16,53 +22,48 @@ const data = [
     requires: [],
   },
   {
+    _id: ObjectId('5ba0f2a2b41162092b59d900'),
     title: 'project 2b',
     description: 'This is the description for project 2b',
     status: 'not started',
     data: 'https://',
     uid: '2',
-    requires: [{
-      title: 'project 1b',
-      status: 'finished',
-    }],
+    requires: [
+      '5ba0f22cb0ad9d0908ece391',
+    ],
   },
   {
+    _id: ObjectId('5ba0f2a2b41162092b59d901'),
     title: 'project 2',
     description: 'This is the description for project 2',
     status: 'not started',
     data: 'https://',
     uid: '1',
-    requires: [{
-      title: 'project 1',
-      status: 'finished',
-    }],
+    requires: [
+      '5b9e1794a626cddcb03a6df5',
+    ],
   },
   {
+    _id: ObjectId('5ba0f2a2b41162092b59d902'),
     title: 'project 3',
     description: 'This is the description for project 3',
     status: 'in progress',
     data: 'https://',
     uid: '1',
-    requires: [{
-      title: 'project 1',
-      status: 'finished',
-    }],
+    requires: [
+      '5b9e1794a626cddcb03a6df5',
+    ],
   },
   {
+    _id: ObjectId('5ba0f2a2b41162092b59d903'),
     title: 'project 4',
     description: 'This is the description for project 4',
     status: 'in progress',
     data: 'https://',
     uid: '1',
     requires: [
-      {
-        title: 'project 1',
-        status: 'finished',
-      },
-      {
-        title: 'project 3',
-        status: 'started',
-      },
+      '5b9e1794a626cddcb03a6df5',
+      '5ba0f2a2b41162092b59d902',
     ],
   },
 ];
