f0b87cc4506ef7cc475583862b70200a7833c12c
Coyla <jcoyla123@gmail.com>
2018-09-21 08:39:30 +0200

    feat(api): add route to get project by id
    necessary to get required projects for frontend

--- rtn-app/server/controllers/project.js
+++ rtn-app/server/controllers/project.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const projectModel = require('../models/project.model');
+const projectHelper = require('../helpers/project');
 
 const router = express.Router();
 
 router.get('', (req, res) => {
   projectModel.find((err, docs) => res.send(docs));
+});
+
+router.get('/:id', (req, res) => {
+  projectModel.findById(req.params.id, async (err, project) => {
+    if (project) {
+      let projectWithRequires = JSON.parse(JSON.stringify(project));
+      if (projectHelper.hasRequiredProjects(project)) {
+        projectWithRequires = await projectHelper.getRequiredProjects(project);
+      }
+      res.status(200);
+      res.send(projectWithRequires);
+    } else {
+      res.status(404).send('not found');
+    }
+  });
 });
 
 module.exports = router;
